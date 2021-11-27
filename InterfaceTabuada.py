9fe3b6233e25e26644f096793d231d6951608596
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2021-11-27 14:37:33 -0300

    Add files via upload

---
+++ InterfaceTabuada.py
@@ -0,0 +1,41 @@
+import PySimpleGUI as sg # pip install PySimpleGui
+
+class TelaPython():
+    def __init__(self):
+        # Tema
+        sg.change_look_and_feel('DarkGrey2')
+
+        # layout
+        layout = [
+            # size = tamanho 
+            # key = nome da chave/atributo
+
+            [sg.Text('Numero',size=(6,0)),sg.Input(size=(20,0),key='Numero')],
+            [sg.Text('Limite',size=(6,0)),sg.Input(size=(20,0),key='Limite')],
+
+            [sg.Button('Enviar Dados')],
+
+            [sg.Output(size=(40,12))]
+        ]
+
+        # Janela
+        self.janela = sg.Window('Tabuada',element_justification='center',size=(300,300)).layout(layout)
+
+    def Iniciar(self):
+        while True:
+            # Dados Extraidos
+            self.button, self.values = self.janela.Read()
+
+            # <varialvel> = self.values[<nome_da_key]
+            numero = self.values['Numero']
+            limite = self.values['Limite']
+            numero = int(numero)
+            limite = int(limite)
+
+            # output na interface
+            print("Tabuada do número ", numero)
+            for valor in range(1,limite+1,1):
+                print(str(numero) + " x " + str(valor) + " = " + str((numero*valor)))
+
+tela = TelaPython()
+tela.Iniciar()
