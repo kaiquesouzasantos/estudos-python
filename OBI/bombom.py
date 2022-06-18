70490b72d87bc6e6dc0237efad6fcf9f2535f5a0
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-18 02:47:08 -0300

    Add files via upload

--- OBI/bombom.py
+++ OBI/bombom.py
@@ -1,7 +1,9 @@
 # 100/100
 
-def somac(figura, naipe, np) -> int:
+def somac(figura, naipe) -> int:
     valor = 0
+    global naipeD
+    np = naipeD
 
     if(figura == "A"):
         if(np == naipe):valor = 14
@@ -17,12 +19,12 @@ def somac(figura, naipe, np) -> int:
         else:valor = 13
     return valor
 
-def soma(figura1, naipe1, figura2, naipe2, figura3, naipe3, np):
-    return somac(figura1, naipe1, np) + somac(figura2, naipe2, np) + somac(figura3, naipe3,np)
+def soma(figura1, naipe1, figura2, naipe2, figura3, naipe3):
+    return somac(figura1, naipe1) + somac(figura2, naipe2) + somac(figura3, naipe3)
 
-l1 = list(input()+" ")
+l1 = input()
 figura = l1[0]
-naipe = l1[1]
+naipeD = l1[1]
 
 l2 = list(input()+" ")
 l3 = list(input()+" ")
@@ -32,9 +34,9 @@ l5 = list(input()+" ")
 l6 = list(input()+" ")
 l7 = list(input()+" ")
 
-if(soma(l2[0], l2[1], l3[0], l3[1], l4[0], l4[1],naipe) > soma(l5[0], l5[1], l6[0], l6[1], l7[0], l7[1], naipe)):
+if(soma(l2[0], l2[1], l3[0], l3[1], l4[0], l4[1]) > soma(l5[0], l5[1], l6[0], l6[1], l7[0], l7[1])):
     print("Luana")
-elif(soma(l2[0], l2[1], l3[0], l3[1], l4[0], l4[1],naipe) < soma(l5[0], l5[1], l6[0], l6[1], l7[0], l7[1],naipe)):
+elif(soma(l2[0], l2[1], l3[0], l3[1], l4[0], l4[1]) < soma(l5[0], l5[1], l6[0], l6[1], l7[0], l7[1])):
     print("Edu")
 else:
     print("empate")
