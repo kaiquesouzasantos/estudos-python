b6c8f6d932a5b25d36f6a2b930b0dc1933386cf4
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-07-02 09:02:40 -0300

    Update batalha.py

--- Competicoes/OBI/batalha.py
+++ Competicoes/OBI/batalha.py
@@ -1,13 +1,7 @@
 # 100/100
 
-a1 = int(input())
-d1 = int(input())
-a2 = int(input())
-d2 = int(input())
+a1, d1, a2, d2 = int(input()), int(input()), int(input()), int(input())
 
-if(a1 != d2 and d1 == a2):
-    print("1\n")
-elif(a2 != d1 and d2 == a1):
-    print("2\n")
-else:
-    print("-1\n")
+if(a1 != d2 and d1 == a2): print("1\n")
+elif(a2 != d1 and d2 == a1): print("2\n")
+else: print("-1\n")
