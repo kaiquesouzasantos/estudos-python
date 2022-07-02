14a9c7a7019ba39edf3609591f2a28d3bedaeae1
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-07-02 08:45:22 -0300

    Update bombom.py

--- Competicoes/OBI/bombom.py
+++ Competicoes/OBI/bombom.py
@@ -1,23 +1,15 @@
 # 100/100
 
-def somac(figura, naipe) -> int:
-    valor = 0
+def somac(figura, naipe):
     global naipeD
     np = naipeD
 
-    if(figura == "A"):
-        if(np == naipe):valor = 14
-        else: valor = 10
-    elif(figura == "J"):
-        if(np == naipe): valor = 15
-        else:valor = 11
-    elif(figura == "Q"):
-        if(np == naipe):valor = 16
-        else:valor = 12
-    elif(figura == "K"):
-        if(np == naipe): valor = 17
-        else:valor = 13
-    return valor
+    if(np == naipe):
+        figuras = {'A':14,'J':15,'Q':16,'K':17}
+        return figuras[figura]
+    else:
+        figuras = {'A': 10, 'J': 11, 'Q': 12, 'K': 13}
+        return figuras[figura]
 
 def soma(figura1, naipe1, figura2, naipe2, figura3, naipe3):
     return somac(figura1, naipe1) + somac(figura2, naipe2) + somac(figura3, naipe3)
@@ -26,17 +18,12 @@ l1 = input()
 figura = l1[0]
 naipeD = l1[1]
 
-l2 = input()
-l3 = input()
-l4 = input()
+l2, l3, l4 = input(), input(), input()
+l5, l6, l7 = input(), input(), input()
 
-l5 = input()
-l6 = input()
-l7 = input()
+soma1 = soma(l2[0], l2[1], l3[0], l3[1], l4[0], l4[1])
+soma2 = soma(l5[0], l5[1], l6[0], l6[1], l7[0], l7[1])
 
-if(soma(l2[0], l2[1], l3[0], l3[1], l4[0], l4[1]) > soma(l5[0], l5[1], l6[0], l6[1], l7[0], l7[1])):
-    print("Luana")
-elif(soma(l2[0], l2[1], l3[0], l3[1], l4[0], l4[1]) < soma(l5[0], l5[1], l6[0], l6[1], l7[0], l7[1])):
-    print("Edu")
-else:
-    print("empate")
+if(soma1 > soma2):print("Luana")
+elif(soma1 < soma2): print("Edu")
+else: print("empate")
