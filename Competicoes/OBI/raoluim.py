d1e835e61078dfed3af8ccb01104d793906fb95e
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-07-02 22:43:10 -0300

    Add files via upload

---
+++ Competicoes/OBI/raoluim.py
@@ -0,0 +1,35 @@
+qtd = int(input())
+melhor = pmelhor = pdist = [0] * (qtd + 1)
+pontos = [[0, 0]]
+
+for i in range(qtd):
+    pontos.append(list(map(int, input().split())))
+
+pares = []
+for a in range(qtd + 1):
+    for b in range(a + 1, qtd + 1):
+        dx = pontos[a][0] - pontos[b][0]
+        dy = pontos[a][1] - pontos[b][1]
+        pares.append([dx * dx + dy * dy, a, b])
+
+pares.sort()
+
+for par in pares:
+    d = par[0]
+    a = par[1]
+    b = par[2]
+
+    if d != pdist[a]:
+        pdist[a] = d
+        pmelhor[a] = melhor[a]
+    if d != pdist[b]:
+        pdist[b] = d
+        pmelhor[b] = melhor[b]
+
+    if a == 0:
+        melhor[a] = max(melhor[a], pmelhor[b])
+    else:
+        melhor[a] = max(melhor[a], 1 + pmelhor[b])
+        melhor[b] = max(melhor[b], 1 + pmelhor[a])
+
+print(1 + melhor[0])
