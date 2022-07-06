cdb9ff57e92b60f8d2ec6c81d193e8c68e0f786a
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-07-06 11:54:16 -0300

    Add files via upload

---
+++ NepsAcademy/frequenciaNaAula.py
@@ -0,0 +1,9 @@
+# 100/100
+
+n = int(input())
+alunos = set()
+
+for i in range(n):
+    alunos.add(int(input()))
+
+print(len(alunos))
---
+++ NepsAcademy/plantacaoMorangos.py
@@ -0,0 +1,6 @@
+# 100/100
+
+aC, aL, bC, bL = int(input()), int(input()), int(input()), int(input())
+calc = [aC*aL, bC*bL]
+
+print(max(calc))
