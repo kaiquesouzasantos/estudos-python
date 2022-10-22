a86dc80efa03736f45878ff600c96501fa984b58
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-10-22 01:29:32 -0300

    Update palindromo.py

--- Competicoes/MaratonaCPS/palindromo.py
+++ Competicoes/MaratonaCPS/palindromo.py
@@ -1,35 +1,23 @@
 import sys
 
-def decToBin(decNum):
-    bin = ""
-    while decNum > 0:
-        resto = decNum % 2
-        decNum = int(decNum/2)
-        bin = bin + str(resto)
-
-    return bin
-
 if __name__ == "__main__":
     while True:            
         linha = input()
-        
         if linha in '0': break
 
         n = int(linha)
 
-        bin = decToBin(n)
+        binario = bin(n)
 
         pal = True
-        tam = len(bin)
+        tam = len(binario)
 
         for x in range(tam):
-            if not bin == bin[::-1]:
+            if not binario == binario[::-1]:
                 pal = False
                 break
 
-        if pal:
-            print("V")
-        else:
-            print("F")
+        if pal: print("V")
+        else: print("F")
             
 sys.exit()
